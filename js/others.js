ee09af3e387ecd2f8ed9077751e6b159894d1a57
mygeshub <161088990+mygeshub@users.noreply.github.com>
2024-04-09 00:20:02 +0100

    Update others.js

--- js/others.js
+++ js/others.js
@@ -1,29 +1,29 @@
 function showAlert() {
   alert("0523838473 | Gtbank | Samuel Winner Ebuka . Once you've paid, tell us, so it will be confirmed immediately.🌬️🌬️");
 }
-              
-               function sendwhatsapp() {
-                 event.preventDefault();
-            var phonenumber = "2347048280029";
-            var name = document.querySelector("#name").value;
-            var email = document.querySelector("#email").value;
-            var pnumber = document.querySelector("#pnumber").value;
-            var university = document.querySelector("#university").value;
-            var department = document.querySelector("#department").value;
-            var level = document.querySelector("#level").value;
-            var ges = document.querySelector("#ges").value;
-            var ref = document.querySelector("#ref").value;
 
-            var url = "https://wa.me/" + phonenumber + "?text=" +
-                "*Name:* " + name + "%0a" +
-                "*Email:* " + email + "%0a" +
-                "*Phone Number:* " + pnumber + "%0a" +
-                "*University:* " + university + "%0a" +
-                "*Department:* " + department + "%0a" +
-                "*Level:* " + level + "%0a" +
-                "*Which GES are you requesting for:* " + ges + "%0a" +
-                "*Refferal code(if any):* " + ref + "%0a" +
-                "Good day, This are my details and I need MyGesHub";
+function sendwhatsapp() {
+  event.preventDefault();
+  var phonenumber = "2347048280029";
+  var name = document.querySelector("#name").value;
+  var email = document.querySelector("#email").value;
+  var pnumber = document.querySelector("#pnumber").value;
+  var university = document.querySelector("#university").value;
+  var department = document.querySelector("#department").value;
+  var level = document.querySelector("#level").value;
+  var ges = document.querySelector("#ges").value;
+  var ref = document.querySelector("#ref").value;
 
-            window.open(url, "_blank").focus();
-        }
+  var url = "https://wa.me/" + phonenumber + "?text=" +
+    "*Name:* " + name + "%0a" +
+    "*Email:* " + email + "%0a" +
+    "*Phone Number:* " + pnumber + "%0a" +
+    "*University:* " + university + "%0a" +
+    "*Department:* " + department + "%0a" +
+    "*Level:* " + level + "%0a" +
+    "*Which GES are you requesting for:* " + ges + "%0a" +
+    "*Refferal code(if any):* " + ref + "%0a" +
+    "Good day, This are my details and I need MyGesHub";
+
+  window.open(url, "_blank").focus();
+    }
